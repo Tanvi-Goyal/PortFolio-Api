65247c3811ee92a9cf7da609c4aef63c1a0a2b3e
Tanvi Goyal <goyaltanvi94@gmail.com>
2018-12-24 13:56:22 +0530

    API Created

--- app.js
+++ app.js
@@ -1,9 +1,32 @@
 const express = require('express');
 const app = express();
-app.use((req , res , next) => {
-    res.status(200).json({
-        message : 'It Works'
-    });
+
+// const myRoutes = require('./api/routes/data');
+
+// app.use('/data' , myRoutes);
+
+const body = require('body-parser');
+
+var admin = require("firebase-admin");
+
+var serviceAccount = require("./security-key.json");
+
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount),
+  databaseURL: "https://myportfolio-7155f.firebaseio.com"
 });
+
+var database = admin.database();
+
+app.use(body.urlencoded({
+    extended: true
+}));
+app.use(body.json());
+
+app.get('/', (req, res) => {
+    database.ref().on('value', function(snapshot) {
+        res.json(snapshot.val())
+    })
+})
 
 module.exports = app;
